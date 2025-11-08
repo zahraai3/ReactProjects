210307d573b71396dd6289ac7014e4c1e7f617f7
zahraai3 <zahraaibrahem241@gmail.com>
2025-11-08 17:24:28 +0300

    funfactofreact-P1/basics of react components

---
+++ Fun-Fact-Of-React/src/components/Header.jsx
@@ -0,0 +1,16 @@
+export default function Header(){
+    return (
+      <>
+        <header>
+          <img src="src/assets/react.svg" />
+          <nav>
+            <ul className='nav-list'>
+              <li className='list-item'>Pricing</li>
+              <li className='list-item'>About</li>
+              <li className='list-item'>Contact</li>
+            </ul>
+          </nav>
+        </header>
+      </>
+    )
+}
---
+++ Fun-Fact-Of-React/src/components/MainContent.jsx
@@ -0,0 +1,15 @@
+export default function MainContent(){
+  return (
+    <>
+      <main>
+        <h1>Fun facts about React</h1>
+        <ol>
+          <li>was released in 2013</li>
+          <li>has well over 100k starts on GITHUB</li>
+          <li>is mentained by meta</li>
+          <li>powerthoousend of enterprise apps,including mobile app</li>
+        </ol>
+      </main>
+    </>
+  )
+}
