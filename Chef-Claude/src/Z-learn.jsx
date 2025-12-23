cf34fe19efd6f41ce51470b31641303ddfadee40
zahraai3 <zahraaibrahem241@gmail.com>
2025-12-23 22:29:58 +0300

    lesson 44- soundpadd chalenge -part 1

--- Chef-Claude/src/Z-learn.jsx
+++ Chef-Claude/src/Z-learn.jsx
@@ -1,45 +1,18 @@
+import padsData from "./components/pads"
 import React from "react"
-import avatar from "./assets/user.png"
-import starFilled from "./assets/star-filled.png"
-import starEmpty from "./assets/star-empty.png"
-import Star from "./components/Star"
 
 export default function Learn() {
-    const [contact, setContact] = React.useState({
-        firstName: "John",
-        lastName: "Doe",
-        phone: "+1 (212) 555-1212",
-        email: "itsmyrealname@example.com",
-        isFavorite: false
-    })
+    const [pads,setpads] = React.useState(padsData)
     
-    let starIcon = contact.isFavorite ? starFilled : starEmpty
-
-    function toggleFavorite() {
-        setContact(prevContact => ({
-            ...prevContact,
-            isFavorite: !prevContact.isFavorite
-        }))
-    }
-
+    const buttonsElement = pads.map(pad => (
+        <button key={pad.id}></button>
+    ))
+    
     return (
         <main>
-            <article className="card">
-                <img
-                    src={avatar}
-                    className="avatar"
-                    alt="User profile picture of John Doe"
-                />
-                <div className="info">
-                    <Star isFavorite={contact.isFavorite} toggleFavorite={toggleFavorite} icon={starIcon}/>
-                    <h2 className="name">
-                        {contact.firstName} {contact.lastName}
-                    </h2>
-                    <p className="contact">{contact.phone}</p>
-                    <p className="contact">{contact.email}</p>
-                </div>
-
-            </article>
+            <div className="pad-container">
+                {buttonsElement}
+            </div>
         </main>
     )
 }
