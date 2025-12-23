db4bd46db0b952a9df4baf1746df14ccf71eecb4
zahraai3 <zahraaibrahem241@gmail.com>
2025-12-23 22:46:25 +0300

    45-Dynamic style

--- Chef-Claude/src/Z-learn.jsx
+++ Chef-Claude/src/Z-learn.jsx
@@ -1,11 +1,15 @@
 import padsData from "./components/pads"
 import React from "react"
 
-export default function Learn() {
+export default function Learn(props) {
     const [pads,setpads] = React.useState(padsData)
+    const style = {
+        backgroundColor : props.darkmode ? "#222222" : "#cccccc"
+    }
     
+
     const buttonsElement = pads.map(pad => (
-        <button key={pad.id}></button>
+        <button key={pad.id} style={style}></button>
     ))
     
     return (
