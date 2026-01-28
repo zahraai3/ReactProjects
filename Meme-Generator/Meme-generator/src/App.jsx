29e45d3c04f74b363e0a5e7cf04ef3582a441b79
zahraai3 <zahraaibrahem241@gmail.com>
2026-01-29 02:47:32 +0300

    L-15:Fetch Meme

--- Meme-Generator/Meme-generator/src/App.jsx
+++ Meme-Generator/Meme-generator/src/App.jsx
@@ -1,26 +1,11 @@
 import Header from "./components/Header";
 import Main from "./components/Main";
 
-import React, { useState } from "react"
-
-export default function App(props) {
-    const [starWarsData, setStarWarsData] = React.useState(null)
-    const[count,setCount] = useState(0)
-    
-    console.log("first console!")
-
-    React.useEffect(() => {
-        fetch("https://swapi.dev/api/people/1")
-        .then(res => res.json())
-        .then(data => setStarWarsData(data))
-        console.log("fetched !!")
-    },[]) 
-    
+export default function App(p) {
     return (
-        <div> 
-          <h1>The count is {count}</h1>
-          <button onClick={() => setCount(prev => prev + 1)}>ADD</button>
-        </div>
+        <>
+            <Header/>
+            <Main/>
+        </>
     )
 }
-{/* <pre>{JSON.stringify(starWarsData, null, 2)}</pre> */}
--- Meme-Generator/Meme-generator/src/components/Main.jsx
+++ Meme-Generator/Meme-generator/src/components/Main.jsx
@@ -1,47 +1,68 @@
-import {useState , useEffect} from "react"
+import { useEffect, useState } from "react"
 
-export default function Main(){
-
-    const [meme,setMeme] = useState({topText:"", buttomText:"" , imageUrl:"http://i.imgflip.com/1bij.jpg"})
+export default function Main() {
+    const [meme, setMeme] = useState({
+        topText: "One does not simply",
+        bottomText: "Walk into Mordor",
+        imageUrl: "http://i.imgflip.com/1bij.jpg"
+    })
     
-    function handleChange(e){
-        const {name,value} = e.currentTarget
-        setMeme(prevMeme =>({
+    function handleChange(event) {
+        const {value, name} = event.currentTarget
+        setMeme(prevMeme => ({
             ...prevMeme,
-            [name]:value
+            [name]: value
         }))
-        console.log(meme[name])
     }
-    return(
+
+    const [memePics,setMemepics] = useState([])
+    useEffect(async () => {
+    const res = await fetch("https://api.imgflip.com/get_memes")
+    const data = await res.json()
+    setMemepics(data.data.memes)
+},[])
+
+
+    function handleMeme(){  
+        if (memePics.length == 0) return
+
+        const randomPic= memePics[Math.floor(Math.random() * memePics.length)]
+        setMeme(prev =>({
+            ...prev,
+            imageUrl:randomPic.url
+        }))
+    }
+
+    return (
         <main>
             <div className="form">
-                <label>Top Text 
-                    <input 
-                        type="text" 
-                        name="topText" 
-                        placeholder={meme.topText} 
+                <label>Top Text
+                    <input
+                        type="text"
+                        placeholder="One does not simply"
+                        name="topText"
                         onChange={handleChange}
                         value={meme.topText}
                     />
                 </label>
 
-                <label>Buttom Text
-                    <input 
-                        type="text" 
-                        name="buttomText"
-                        placeholder={meme.buttomText}
+                <label>Bottom Text
+                    <input
+                        type="text"
+                        placeholder="Walk into Mordor"
+                        name="bottomText"
                         onChange={handleChange}
-                        value={meme.buttomText}
+                        value={meme.bottomText}
                     />
                 </label>
-
-                <button>Get a new meme image 🖼</button>
+                <button  onClick={handleMeme}>Get a new meme image 🖼</button>
             </div>
             <div className="meme">
                 <img src={meme.imageUrl} />
                 <span className="top">{meme.topText}</span>
-                <span className="bottom">{meme.buttomText}</span>
+                <span className="bottom">{meme.bottomText}</span>
             </div>
         </main>
     )
 }
+
