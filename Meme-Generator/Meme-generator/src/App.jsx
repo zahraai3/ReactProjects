28b66354a2d493a896e7d54996e6eb0e771560c5
zahraai3 <zahraaibrahem241@gmail.com>
2026-01-28 21:11:50 +0300

    L-10: useEffect( syntax

--- Meme-Generator/Meme-generator/src/App.jsx
+++ Meme-Generator/Meme-generator/src/App.jsx
@@ -1,11 +1,26 @@
 import Header from "./components/Header";
 import Main from "./components/Main";
 
-export default function App(){
-  return(
-    <>
-      <Header/>
-      <Main/>
-    </>
-  )
+import React, { useState } from "react"
+
+export default function App(props) {
+    const [starWarsData, setStarWarsData] = React.useState(null)
+    const[count,setCount] = useState(0)
+    
+    console.log("first console!")
+
+    React.useEffect(() => {
+        fetch("https://swapi.dev/api/people/1")
+        .then(res => res.json())
+        .then(data => setStarWarsData(data))
+        console.log("fetched !!")
+    },[]) 
+    
+    return (
+        <div> 
+          <h1>The count is {count}</h1>
+          <button onClick={() => setCount(prev => prev + 1)}>ADD</button>
+        </div>
+    )
 }
+{/* <pre>{JSON.stringify(starWarsData, null, 2)}</pre> */}
