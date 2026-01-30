3140d6be54b89f2362ad36026903d6e089f3206c
zahraai3 <zahraaibrahem241@gmail.com>
2026-01-30 19:36:27 +0300

    L-14: adding a win effect

--- Tenzies-Game/src/App.jsx
+++ Tenzies-Game/src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Die from "./components/Die";
 import {nanoid} from "nanoid"
+import Confetti from "react-confetti"
 
 export default function App(){
 
@@ -46,6 +47,7 @@ export default function App(){
 
     return(
         <main>
+            {gameWon && <Confetti/>}
             <h1 className="title">Tenzies</h1>
             <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
 
