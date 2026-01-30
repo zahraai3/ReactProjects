b30607df0373e6ff77e965d0f3416ebcb5762722
zahraai3 <zahraaibrahem241@gmail.com>
2026-01-30 21:01:30 +0300

    L-17 : Accessibility Improvment

--- Tenzies-Game/src/App.jsx
+++ Tenzies-Game/src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import Die from "./components/Die";
 import {nanoid} from "nanoid"
 import Confetti from "react-confetti"
@@ -6,6 +6,8 @@ import Confetti from "react-confetti"
 export default function App(){
 
     const [dice,setDice] = React.useState(generateAllNewDice)
+    const buttonRef = useRef(null)
+
 // كل مرة component يعيد render، الكود كله ينكتب من جديد.
 
 // إذا initial state كان فنكشن ثقيل ونفذته فوراً → كل render راح يشتغل زيادة.
@@ -59,17 +61,26 @@ export default function App(){
     )
 }
 
+//مجرد اضافه للمستخدم تسهيل يروح مباشره لزر لعبه جديده
+useEffect(() => {
+    if(gameWon){
+        buttonRef.current.focus()
+    }
+},[gameWon])
 
     return(
         <main>
             {gameWon && <Confetti/>}
+            <div aria-live="polite" className="sr-only">
+                {gameWon && <p>Congratulations you WON! Press "New Game" to start again</p>}
+            </div>
             <h1 className="title">Tenzies</h1>
             <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
 
             <div className="container">
                 {DiceElements}
             </div>
-            <button className="roll-dice " onClick={rollDice}>{gameWon ? "New Game" : "Roll"}</button>
+            <button  ref={buttonRef} className="roll-dice " onClick={rollDice}>{gameWon ? "New Game" : "Roll"}</button>
         </main>
     )
 }
--- Tenzies-Game/src/components/Die.jsx
+++ Tenzies-Game/src/components/Die.jsx
@@ -2,6 +2,17 @@
 
 export default function Die(props){
     return(
-        <button style={{backgroundColor: props.isHeld ? "#59E391" : "#f5f5f5"}} onClick={()=>props.holdFunc(props.id)}>{props.value}</button>
+        <button 
+            style={
+                {backgroundColor: 
+                    props.isHeld ? "#59E391" : "#f5f5f5"}
+                } 
+            onClick={
+                ()=>props.holdFunc(props.id)
+            }
+            aria-label={`die with value of ${props.value}, ${props.isHeld ? "held" : "not held"}`}
+            aria-pressed={props.isHeld}
+            >            
+        {props.value}</button>
     )
 }
