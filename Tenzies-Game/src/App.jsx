d0f21df3adbb3e598b740c39e7bc61cae4184f0b
zahraai3 <zahraaibrahem241@gmail.com>
2026-01-30 14:03:43 +0300

    L-1,2,3 : Tenzies game setup

--- Tenzies-Game/src/App.jsx
+++ Tenzies-Game/src/App.jsx
@@ -1,1 +1,22 @@
+import React from "react";
+import Die from "./components/Die";
 
+export default function App(){
+
+    return(
+        <main>
+            <div className="container">
+                <Die value={1}/>
+                <Die value={1}/>
+                <Die value={1}/>
+                <Die value={1}/>
+                <Die value={1}/>
+                <Die value={1}/>
+                <Die value={1}/>
+                <Die value={1}/>
+                <Die value={1}/>
+                <Die value={1}/>
+            </div>
+        </main>
+    )
+}
---
+++ Tenzies-Game/src/components/Die.jsx
@@ -0,0 +1,7 @@
+
+
+export default function Die(props){
+    return(
+        <button>{props.value}</button>
+    )
+}
