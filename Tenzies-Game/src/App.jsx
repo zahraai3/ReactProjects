d58515dd72de912c939c8ef0953b14c3b7158840
zahraai3 <zahraaibrahem241@gmail.com>
2026-01-30 15:31:26 +0300

    L-7:Change dice to objects and adding a uniqe id

--- Tenzies-Game/src/App.jsx
+++ Tenzies-Game/src/App.jsx
@@ -1,26 +1,28 @@
 import React from "react";
 import Die from "./components/Die";
+import {nanoid} from "nanoid"
 
 export default function App(){
 
-    const [dice,setDice] = React.useState(allNewDice)
+    const [dice,setDice] = React.useState(generateAllNewDice)
 
-    function allNewDice(){
+    function generateAllNewDice(){
         const numsArr = []
         for (let i=0;i<10;i++){
             const rand = Math.floor(Math.random() * 6)+1
-            numsArr.push(rand)
+            const obj = {value: rand , isHeld:false , id: nanoid() }
+            numsArr.push(obj)
         }
         return numsArr
     }
-    console.log(allNewDice( ))
+    console.log(generateAllNewDice())
 
-    const DiceElements = dice.map((num)=>{
-        return <Die value={num} />
+    const DiceElements = dice.map((diceObj)=>{
+        return <Die value={diceObj.value} key={diceObj.id}/>
     })
 
     function rollDice(){
-        setDice(allNewDice)
+        setDice(generateAllNewDice)
     }
 
     return(
@@ -28,7 +30,7 @@ export default function App(){
             <div className="container">
                 {DiceElements}
             </div>
-            <button className="roll-dice" onClick={rollDice}>Roll</button>
+            <button className="roll-dice " onClick={rollDice}>Roll</button>
         </main>
     )
 }
