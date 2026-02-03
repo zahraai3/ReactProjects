855185646032b5bf6bf169bdb0e830d9488c14fd
zahraai3 <zahraaibrahem241@gmail.com>
2026-02-03 14:15:40 +0300

    L-5 : added the langs chips using map()

--- Assembly-Endgame/src/App.jsx
+++ Assembly-Endgame/src/App.jsx
@@ -1,11 +1,36 @@
 import Header from "./components/Header"
 import Main from "./components/Main"
+import { languages  } from "./assets/languages"
 
 export default function App(){
+
+    const langElements = languages.map((item) => (
+        <span className="chip"
+            key={item.name}
+            style={ {
+                    backgroundColor:item.backgroundColor ,
+                    color:item.color
+                }}
+        > 
+            {item.name}
+        </span>
+    ))
+
     return(
         <>
-            <Header/>
-            <Main/>
+            <main>
+                <header>
+                    <h1>Assembly: Endgame</h1>
+                    <p>Guess the word in under 8 attempts to keep the programming world safe from Assembly!</p>
+                </header>
+                <section className="game-status">
+                    <h2>You win!</h2>
+                    <p>Well done! 🎉</p>
+                </section>
+                <section className="language-chips">
+                    {langElements}
+                </section >
+            </main>
         </>
     )
 }
